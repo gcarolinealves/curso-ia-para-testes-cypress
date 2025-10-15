7161d7db2a1ee827f9aea3921ee5d84e6e95d355
Caroline Sacramento Alves <caroline.alves@MMB040529BHZ.local>
2025-10-15 00:29:31 -0300

    Alterando o nome das variáveis

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,13 +3,13 @@
 describe("Login", () => {
     beforeEach(() => {
         cy.visit("https://www.demoblaze.com/")
-        cy.fixture('usuarios').as('credenciais')
+        cy.fixture('users').as('credentials')
     })
 
     it("Successful login", () => {
-        cy.get('@credenciais').then((credenciais) => {
-            cy.login(credenciais.usernameCadastrado, credenciais.passwordCadastrado)
-            cy.get("#nameofuser").should("be.visible").should("have.text", `Welcome ${credenciais.usernameCadastrado}`)
+        cy.get('@credentials').then((credentials) => {
+            cy.login(credentials.validUsername, credentials.validPassword)
+            cy.get("#nameofuser").should("be.visible").should("have.text", `Welcome ${credentials.validUsername}`)
         });
-    });
-});
+    })
+})
