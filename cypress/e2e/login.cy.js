db1c8458dcc3a387f2ee9ba29cbdf5b203544861
Caroline Sacramento Alves <caroline.alves@MMB040529BHZ.local>
2025-10-15 00:36:51 -0300

    Login com campos vazios

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,10 +13,17 @@ describe("Login", () => {
         });
     })
 
-     it("Invalid password", () => {
+    it("Invalid password", () => {
         cy.get('@credentials').then((credentials) => {
             cy.login(credentials.validUsername, credentials.invalidPassword)
-            cy.on('window:alert', (alertText) => { expect(alertText).to.eq('User does not exist.'); });
+            cy.on('window:alert', (alertText) => { expect(alertText).to.eq("User does not exist."); });
         });
+    })
+
+    it("Empty fields ", () => {
+        cy.get("#login2").click()
+        cy.contains('button', 'Log in').click()    
+        cy.on('window:alert', (alertText) => { expect(alertText).to.eq("Please fill out Username and Password."); });
+        
     })
 })
